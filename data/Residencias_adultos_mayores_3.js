5548d8b18ec6b4b543d5e3eca64a178a5424df18
planiflab <58193669+planiflab@users.noreply.github.com>
2020-05-29 11:59:34 -0300

    Add files via upload

---
+++ data/Residencias_adultos_mayores_3.js
@@ -0,0 +1,17 @@
+var json_Residencias_adultos_mayores_3 = {
+"type": "FeatureCollection",
+"name": "Residencias_adultos_mayores_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": "1", "RESIDENCIA": "\"Mi Hogar\" Residencia para Se�oras Mayores", "DIRECCION_": "Hipolito Yrigoyen 172 -GC", "TELEFONO": "0261-4244800 Sra Beatriz 155038731", "CANT__DE_C": 14.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.845886591943952, -32.90370088268569 ] } },
+{ "type": "Feature", "properties": { "ID": "2", "RESIDENCIA": "\"Residencia del Sol\"", "DIRECCION_": "Pte Alvear 375 - GC", "TELEFONO": "0261- 4242368 Pablo Enrique 156567089", "CANT__DE_C": 9.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.839870525827479, -32.912710008286489 ] } },
+{ "type": "Feature", "properties": { "ID": "3", "RESIDENCIA": "\"La Resistencia\"", "DIRECCION_": "Santiago del Estero 1650-GC", "TELEFONO": "0261- 4271692", "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.86293776065834, -32.902773189426831 ] } },
+{ "type": "Feature", "properties": { "ID": "4", "RESIDENCIA": "\"Raices\" Residencia de la Tercera Edad", "DIRECCION_": "Elena de Roffo 2502 -GC", "TELEFONO": "261-4284305", "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.870345030916766, -32.916333465196274 ] } },
+{ "type": "Feature", "properties": { "ID": "5", "RESIDENCIA": "Casa de los Abuelos", "DIRECCION_": "Derqui esq. Marmol", "TELEFONO": null, "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.857189233799886, -32.916776654703476 ] } },
+{ "type": "Feature", "properties": { "ID": "6", "RESIDENCIA": "\"Mis Abuelos\" Residencia", "DIRECCION_": "Av. San Martin 390-GC", "TELEFONO": "261-4245230", "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.847218045651616, -32.920036125566753 ] } },
+{ "type": "Feature", "properties": { "ID": "7", "RESIDENCIA": "Geriatrico", "DIRECCION_": "Col�n 185", "TELEFONO": null, "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.845587839772421, -32.926722381371285 ] } },
+{ "type": "Feature", "properties": { "ID": "8", "RESIDENCIA": "\"Manantiales\" Residencia para adultos Mayores", "DIRECCION_": "Benavente 840 - GC", "TELEFONO": "0261-4363167", "CANT__DE_C": 18.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.833837515558002, -32.936078698020729 ] } },
+{ "type": "Feature", "properties": { "ID": "9", "RESIDENCIA": "\"El Jardin de mis Abuelos\"", "DIRECCION_": "Cerro Trapal 1240 - GC", "TELEFONO": "0261-4521007 Julio Bencinelli", "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.827192286286262, -32.943191081996858 ] } },
+{ "type": "Feature", "properties": { "ID": "10", "RESIDENCIA": "\"Hogar Suizo\"", "DIRECCION_": "Elena de Zizzias 699 - GC", "TELEFONO": "0261-4392763", "CANT__DE_C": 0.0, "DISPONIBIL": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -68.844748212400901, -32.954554889908529 ] } }
+]
+}
